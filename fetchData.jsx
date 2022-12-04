bd6a45a267b5e1215c8fd319fa015d57da725172
tybo-dev <tobias@diez.guru git config --global user.name tgit config --global user.email tobias@diez.guru>
2022-12-04 14:30:35 +0100

    working breed url

--- fetchData.jsx
+++ fetchData.jsx
@@ -2,23 +2,37 @@ function App() {
   const { useState, useEffect } = React;
   const { Container } = ReactBootstrap;
   const [imageUrl, setImageUrl] = useState("");
+  const [breed, setBreed] = useState("beagle");
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios("https://dog.ceo/api/breeds/image/random");
-      console.log(result);
+      //console.log(result);
       setImageUrl(result.data.message);
     };
     fetchData();
-  }, []);
+  },[]);
 
-  console.log("imgage:" + imageUrl);
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(breed);
+      const breedResult = await axios(`https://dog.ceo/api/breed/${breed}/images/random`);
+      console.log("inside breed: " + breedResult.data.message);
+      setBreed(breedResult.data.message);
+    };
+    fetchData();
+  },[]);
+
+ // console.log("imgage:" + imageUrlBreed);
 
   return (
     <Container>
       <div>
+{/*         
         <p>{imageUrl}</p>
-        <img src={imageUrl}></img>
+        <img src={imageUrl}></img> */}
+        <p>{breed}</p>
+        <img src={breed}></img>
       </div>
     </Container>
   );
