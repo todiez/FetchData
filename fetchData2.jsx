ff5ff332ee7314bb691d589e2b2135bd6e84bbd8
tybo-dev <tobias@diez.guru git config --global user.name tgit config --global user.email tobias@diez.guru>
2022-12-05 16:11:24 +0100

    randomized breed function added

--- fetchData2.jsx
+++ fetchData2.jsx
@@ -1,67 +1,98 @@
 const { Button } = ReactBootstrap;
 
-const BreedButton = () => {
-
- 
+const BreedButton = ({ breedChange, breedListOrigin }) => {
+  const breedList = breedListOrigin.map((breed) => {
     return (
-      <Button>Beagle</Button>
+      <Button key={breed} onClick={breedChange}>
+        {breed}
+      </Button>
     );
-  
-};
+  });
 
+  return (
+    <nav>
+      <ul className="pagination">{breedList}</ul>
+    </nav>
+  );
+};
 
 function App() {
   const { useState, useEffect } = React;
   const { Container } = ReactBootstrap;
   const [imageUrl, setImageUrl] = useState("");
   const [breed, setBreed] = useState("beagle");
-  const { Button } = ReactBootstrap;
-  
+  const [breedList, setBreedList] = useState([
+    "beagle",
+    "african",
+    "boxer",
+    "retriever",
+    "leonberg",
+  ]);
+
+  //get random image from breed
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("https://dog.ceo/api/breeds/image/random");
-      //console.log(result);
-      setImageUrl(result.data.message);
+      try {
+        const breedResult = await axios(
+          `https://dog.ceo/api/breed/${breed}/images/random`
+        );
+        setBreed(breedResult.data.message);
+      } catch (error) {
+        console.log("basst scho");
+      }
     };
     fetchData();
-  },[]);
+  }, [breed]);
 
+  //get original breed list and pic random 4
   const trigger = () => {
-    const fetchData = async () => {
-      const result = await axios("https://dog.ceo/api/breeds/image/random");
-      //console.log(result);
-      setImageUrl(result.data.message);
+    const breedListForWork = [];
+    const breedListTemp = [];
+    const fetchBreedList = async () => {
+      try {
+        const breedListOrigin = await axios(
+          "https://dog.ceo/api/breeds/list/all"
+        );
+        //Object key to array
+        for (var property in breedListOrigin.data.message) {
+          breedListForWork.push(property);
+        }
+        //strip down complete array to 5 buttons
+        for (let i = 0; i < 5; i++) {
+          breedListTemp.push(breedListForWork[Math.floor(Math.random() * 98)]);
+        }
+        setBreedList(breedListTemp);
+      } catch (error) {
+        console.log("basst scho trigger");
+      }
     };
-    fetchData();
-  }
+    fetchBreedList();
+  };
 
-
-  useEffect(() => {
-    const fetchData = async () => {
-      console.log(breed);
-      const breedResult = await axios(`https://dog.ceo/api/breed/${breed}/images/random`);
-      console.log("inside breed: " + breedResult.data.message);
-      setBreed(breedResult.data.message);
-    };
-    fetchData();
-  },[]);
-
- // console.log("imgage:" + imageUrlBreed);
+  const handleBreedChange = (e) => {
+    setBreed(e.target.textContent);
+  };
 
   return (
-    
     <Container>
-      <BreedButton></BreedButton>
+      <h1>Choose your favorite breed:</h1>
+      <BreedButton
+        breedChange={handleBreedChange}
+        breedListOrigin={breedList}
+      ></BreedButton>
+
+      <input
+        type="button"
+        value="Randomize Breeds"
+        onClick={(e) => trigger()}
+      ></input>
+      <br />
+      <br />
       <div>
-      <input type="button" value="Click me for Random Dog" onClick={(e) => trigger()}></input>
-        <p>{imageUrl}</p>
-        <img src={imageUrl}></img>
-        <p>{breed}</p>
-        <img src={breed}></img>
-        
+        <img src={breed}
+        width="800"
+        ></img>
       </div>
-
-
     </Container>
   );
 }
